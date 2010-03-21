98a41230827bd967d730965cbcbdc853fbeeccd5
Matthew McCullough <matthewm@ambientideas.com>
2010-03-21 12:42:36 -0600

    Making sample02b compile with JUnit 4.7, source code level 1.5 with annotations

--- sample02b-analyzedepmgt/src/test/java/com/ambientideas/AppTest.java
+++ sample02b-analyzedepmgt/src/test/java/com/ambientideas/AppTest.java
@@ -1,36 +1,18 @@
 package com.ambientideas;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
         assertTrue( true );
