88df107fa858ad8088def284cb2a0c8f5c38411d
Matthew McCullough <matthewm@ambientideas.com>
2010-03-31 22:51:54 -0600

    Added range failure example from Anders Hammar

---
+++ sample27-rangefailure/module1/src/main/java/org/module1/App.java
@@ -0,0 +1,13 @@
+package org.module1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
